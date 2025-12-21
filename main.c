735a5416461cf298dc6928649c6fa925e7af10ff
jai-git4208 <jaiminpansal@gmail.com>
2025-12-21 08:59:46 -0800

    create simple pomodoro timer

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+typedef enum {w, b} State;
+#define wd (25 * 6)
+#define bd (5 * 60)
+
+void printTimer(State state, int sec){
+    int min = sec / 60;
+    int secs = sec % 60;
+    const char *state_str = (state == w) ? "WORK" : "BREAK";
+    printf("\r%s : %02d:%02d", state_str, min, secs);
+    fflush(stdout);
+}
+
+int main(){
+    State current = w;
+    while(1){
+        int duration = (current == w) ? wd : bd;
+
+        for(int i = duration; i >= 0; i--){
+            printTimer(current, i);
+            sleep(1);
+
+        }
+        print("\n");//new lien
+
+        //state change
+        if (current == w){
+            printf("Break!\n");
+            current = b;
+        }
+        else {
+            printf("work time! \n");
+            current = w;
+        }
+    }
+    return 0;
+}
